119091417396043c1051cebcff220ef9ad136101
kadadot216 <yoctopolochoon54@gmail.com>
2018-09-25 15:35:36 +0400

    Fixing getting lidar info

--- src/main.c
+++ src/main.c
@@ -15,15 +15,16 @@
 #include "command.h"
 #include "my.h"
 
-int	n4s_track_cleared(callback_t *c)
+int	n4s_track_cleared(callback_t *c, command_t *siminfo)
 {
+	c = callback_set_rtype(c, RES_SIMTIME);
+	callback_getcmd(c, siminfo);
+	dprintf(2, "%s\n", c->addinfo);
 	return (my_strn_eq(INFO_TRACK, c->addinfo));
 }
 
 int	main(int ac, char **av)
 {
-	char	*line = NULL;
-	size_t	n = 0;
 	callback_col_t	collection = callback_col_new();
 	command_t	*simtab = get_simtab();
 	callback_t	c = callback_new();
@@ -32,29 +33,22 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	callback_link_ref(&c, &collection);
-	command_send(&simtab[START_SIMULATION]);
-	getline(&line, &n, stdin);
-	callback_get_parts(&c, line);
-	callback_print_all(&c);
+	callback_getcmd(&c, &simtab[START_SIMULATION]);
+	callback_set_rtype(&c, RES_SIMTIME);
 	sleep(2);
 	vehicle_set_speed(&vehicle, 0.5);
 	vehicle_update_actions(&vehicle);
-	command_send(&vehicle.action[CAR_FORWARD]);
-	getline(&line, &n, stdin);
-	callback_get_parts(&c, line);
-	//callback_print_all(&c);
-	while (!n4s_track_cleared(&c)) {
-		vehicle_getinfos(&vehicle, &collection);
-		usleep(100);
-		command_send(&simtab[GET_INFO_SIMTIME]);
-		usleep(100);
-		getline(&line, &n, stdin);
-		callback_get_parts(&c, line);
+	callback_set_rtype(&c, RES_NIL);
+	callback_getcmd(&c, &vehicle.action[CAR_FORWARD]);
+	while (!n4s_track_cleared(&c, &simtab[GET_INFO_SIMTIME])) {
+		vehicle_observe(&vehicle, &c);
+		print_vehicle_infos(&vehicle);	//dbg
 		//callback_print_all(&c);
-		usleep(200);
+		usleep(20);
 	}
 	callback_print_all(&c);
-	free(line);
+	usleep(2000);
+	callback_getcmd(&c, &simtab[STOP_SIMULATION]);
 	simtab = ctab_destroy(simtab, SIM_ACTIONS_SIZE);
 	vehicle_destroy(&vehicle);
 	callback_unset(&c);
